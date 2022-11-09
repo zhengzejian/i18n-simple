3bd23f1810bee8c4b6e99cdb8adff2fbc28d5c0c
郑泽坚 <zhengzejian@dianxiaomi.com>
2022-11-09 21:26:51 +0800

    base complete

--- src/core/SourceData.ts
+++ src/core/SourceData.ts
@@ -1,27 +1,24 @@
-const glob = require('glob');
 const path = require('path');
 const fs = require('fs');
 const { Config } = require('./Config');
-import { workspace, RelativePattern, FileSystemWatcher } from 'vscode';
+import { workspace, RelativePattern, FileSystemWatcher, Uri } from 'vscode';
 
 export class SourceData {
 
     static translateDataMap = {};
     static watcher: FileSystemWatcher;
 
-    static update() {
+    static async update() {
 
         let rootPath = SourceData.getRootPath();
         SourceData.translateDataMap = {};
         if (!rootPath) return;
-
+        const translateFiles = await workspace.findFiles(new RelativePattern(rootPath as string, Config.localesPaths));
         if (!Config.localesPaths) return;
         const tempDir = fs.mkdtempSync('translateData');
-        const translateFiles = glob.sync(Config.localesPaths, { cwd: rootPath });
         const TEMP_FILE = path.resolve(tempDir, 'TEMP.js');
-        translateFiles.forEach(async (file: String) => {
-            let filePath = path.resolve(rootPath, file);
-            let content = fs.readFileSync(filePath, 'utf-8');
+        translateFiles.forEach(async (item: Uri) => {
+            let content = fs.readFileSync(item.fsPath, 'utf-8');
             content = content.replace('export default', 'module.exports = ');
             try {
                 fs.writeFileSync(TEMP_FILE, content);
@@ -31,6 +28,7 @@ export class SourceData {
             Object.assign(SourceData.translateDataMap, data);
         });
         SourceData.watchFile();
+        fs.rm(tempDir, { recursive: true, force: true }, (err: any) => err && console.log(`TEMP File deletion failed: ${err}`));
     }
 
     static watchFile() {
