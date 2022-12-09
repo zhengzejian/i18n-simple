0bafe779593efa29ad4d18737eec53473e8aa0a7
郑泽坚 <120657866@qq.com>
2022-12-09 17:50:00 +0800

    feat:  add onDidChangeActiveTextEditor listener

--- src/extension.ts
+++ src/extension.ts
@@ -7,14 +7,14 @@ import { debounceTranslate } from './core/Translator';
 export function activate(context: ExtensionContext) {
 	SourceData.update();
 
-	workspace.onDidChangeTextDocument(debounceTranslate);
-	workspace.onDidOpenTextDocument(debounceTranslate);
-	workspace.onDidChangeConfiguration(SourceData.update);
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 
-	// context.subscriptions.push(disposable);
+	let disposableChangeActive = window.onDidChangeActiveTextEditor(debounceTranslate);
+	let disposableChangeText = workspace.onDidChangeTextDocument(debounceTranslate);
+	let disposableChangeOpen = workspace.onDidOpenTextDocument(debounceTranslate);
+	let disposableChangeConfig = workspace.onDidChangeConfiguration(SourceData.update);
+
+	[disposableChangeActive, disposableChangeText, disposableChangeOpen, disposableChangeConfig]
+		.forEach(disposable => context.subscriptions.push(disposable));
 }
 
 // This method is called when your extension is deactivated
